6bd0d132024a0311a5f3817d6e7118ef4f99a785
Aakash Singh <aakash_kumar_singh@outlook.com>
2025-06-07 19:35:16 +0530

    Structure, exception and logging

--- Projects/End_2_End_ML/readme.md
+++ Projects/End_2_End_ML/readme.md
@@ -1,4 +1,3 @@
-Set up project with git hub
 1. Data Ingestion
 2. Data Transformation
 3. Model Trainer
@@ -6,3 +5,20 @@ Set up project with git hub
 5. Model Deployment (on AWS)
 
 CI/CD Pipelines with Github Actions
+
+Step 1: Set up project with git hub
+Connect with your git hub repo with a readme and gitignore
+Created src folder with "__init__.py"
+Created setup.py
+Created Requirements.txt, added -e . to connect it with setup.py to run automatically
+Run pip install requirements -> installs requirements and automatically builds package from setup.py
+A new package folder egg-info appears -> added this to gitignore.
+Push it to all to repo.
+
+Step 2: Structure, logging and exception
+
+
+Python modules reference to read and learn:
+1. setup
+2. sys
+3. custom exception handling
---
+++ Projects/End_2_End_ML/src/exception.py
@@ -0,0 +1,32 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Returns a detailed error message.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = "Error occurred in script: [{file_ name}] at line number: [{line_number}] with message: [{str(error)}]"
+    
+
+class CustomException(Exception):
+    """
+    Custom exception class to handle exceptions with detailed error messages.
+    """
+    def __init__(self, error, error_detail: sys):
+        super().__init__(error_message_detail(error, error_detail))
+        self.error_message = error_message_detail(error, error_detail)
+
+        def __str__(self):
+            return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1 / 0  # Example to raise an exception
+    except Exception as e:
+        logging.info("Divide by zero exception occurred")
+        raise CustomException(e, sys) from e
+
+        
