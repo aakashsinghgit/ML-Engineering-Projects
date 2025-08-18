19765c67a67dd32986aaa3e56314528253462fcd
Aakash Singh <aakash_kumar_singh@outlook.com>
2025-08-18 20:41:08 +0530

    fixed deployment issues

--- Projects/P1_Loan_Criterion/main.py
+++ Projects/P1_Loan_Criterion/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
